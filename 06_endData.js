44e1a6e9c9cb0354c89a3302fc69a2f8b6287403
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2017-06-04 19:07:58 +0200

    Thos example is no longer needed

--- 06_endData.js
+++
@@ -1,9 +0,0 @@
-
-res.write('Some Data!!');
-res.end('Ended.');
-When end() is called and every chunk of data has been flushed, a finish event is emitted by the stream. Just note that you can’t write to the stream after calling end(). For example, the following will result in an error.
-
-res.write('Some Data!!');
-res.end();
-res.write('Trying to write again'); //Error!
-Here are some important events related to writable streams:
